f174242c84487282b6484f26bdad81d2d20e6e11
Louis Born <louisborn12@gmail.com>
2022-11-05 13:42:35 +0100

    initial setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AssessmentComponent } from './groups/assessment/assessment.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'assessment', component: AssessmentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AssessmentComponent } from './groups/assessment/assessment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AssessmentComponent,
   ],
   imports: [
     BrowserModule,
