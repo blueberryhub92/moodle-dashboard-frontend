baf6cd4ca5ca589653d312f7fab8600703749769
Raphael Stedler <r.stedler@gmx.de>
2022-11-08 11:11:34 +0100

    grade api

---
+++ src/app/grades/grade.model.ts
@@ -0,0 +1,9 @@
+export class Grade {
+    constructor(
+      public _id?: number,
+      public quiz?: number,
+      public userid?: number,
+      public grade?: number,
+      public timemodified?: number,
+    ) { }
+  }
---
+++ src/app/grades/grades-api.service.ts
@@ -0,0 +1,17 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+import {API_URL} from '../env';
+import {Grade} from './grade.model';
+
+@Injectable()
+export class GradesApiService {
+
+  constructor(private http: HttpClient) {
+  }
+  // GET list of public, future events
+  getGrades(): Observable<Grade[]>{
+    return this.http.get<Grade[]>(`${API_URL}/api`);
+    }
+  }
