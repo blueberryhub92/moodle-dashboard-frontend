de901312ac7dbf0e931b037e56fc4db00410660b
Raphael Stedler <r.stedler@gmx.de>
2022-11-16 15:36:00 +0100

    returned to mockup

--- src/app/groups/dashboard/dashboard.component.ts
+++ src/app/groups/dashboard/dashboard.component.ts
@@ -1,9 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Chart } from 'angular-highcharts';
-import { donutChartOptions } from './helpers/donutChartOptions';
-import { areaChartOptions } from './helpers/areaChartOptions';
-import { barChart } from './helpers/barChart';
-import { oneLineBar } from './helpers/oneLineBar';
 
 @Component({
   selector: 'app-dashboard',
@@ -11,10 +6,6 @@ import { oneLineBar } from './helpers/oneLineBar';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-  chart = new Chart(donutChartOptions);
-  areaSplineChart = new Chart(areaChartOptions);
-  barChart = new Chart(barChart);
-  oneLineBar = new Chart(oneLineBar);
 
   constructor() { }
 
