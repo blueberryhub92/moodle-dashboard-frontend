4d487ec14cd9026e50d33592cb97e4d2e62b84fe
Raphael Stedler <r.stedler@gmx.de>
2022-11-16 16:13:03 +0100

    new npm install

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,7 +18,6 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
-import { ChartModule } from 'angular-highcharts';
 
 @NgModule({
   declarations: [
@@ -40,7 +39,6 @@ import { ChartModule } from 'angular-highcharts';
     MatButtonModule,
     MatIconModule,
     MatDividerModule,
-    ChartModule
     // AppRoutingModule
   ],
   providers: [GradesApiService],
